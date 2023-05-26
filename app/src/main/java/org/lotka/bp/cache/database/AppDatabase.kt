d4410b8e8c48ee8e3711523cc1634ffd789b5be6
Mobin Jafari <admin@lotka.org>
2023-05-26 14:54:07 +0330

    Success New Files added with new Gradle version 8.0.1

---
+++ app/src/main/java/org/lotka/bp/cache/database/AppDatabase.kt
@@ -0,0 +1,19 @@
+package org.lotka.bp.cache.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import org.lotka.bp.cache.RecipeDao
+import org.lotka.bp.cache.model.RecipeEntity
+
+@Database(entities = [RecipeEntity::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun recipeDao(): RecipeDao
+
+    companion object {
+        const val DATABASE_NAME: String = "recipe_db"
+
+    }
+
+
+}
