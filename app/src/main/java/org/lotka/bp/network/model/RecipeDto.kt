1976fa9818324b589d6424aa43bec9f6e533488b
Mobin Jafari <admin@lotka.org>
2023-05-26 14:56:03 +0330

    Add New Files

---
+++ app/src/main/java/org/lotka/bp/network/model/RecipeDto.kt
@@ -0,0 +1,45 @@
+package org.lotka.bp.network.model
+
+import com.google.gson.annotations.SerializedName
+
+data class RecipeDto(
+
+    @SerializedName("pk")
+    var pk: Int,
+
+    @SerializedName("title")
+    var title: String,
+
+    @SerializedName("publisher")
+    var publisher: String,
+
+    @SerializedName("featured_image")
+    var featuredImage: String,
+
+    @SerializedName("rating")
+    var rating: Int = 0,
+
+    @SerializedName("source_url")
+    var sourceUrl: String,
+
+    @SerializedName("ingredients")
+    var ingredients: List<String> = emptyList(),
+
+    @SerializedName("long_date_added")
+    var longDateAdded: Long,
+
+    @SerializedName("long_date_updated")
+    var longDateUpdated: Long,
+)
+
+
+
+
+
+
+
+
+
+
+
+
