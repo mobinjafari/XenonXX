7f360989efc21eebcf2567379fc807e0c3097b40
Mobin Jafari <admin@lotka.org>
2023-05-26 14:56:24 +0330

    Add New Files

---
+++ app/src/main/java/org/lotka/bp/interactors/recipe_list/RestoreRecipes.kt
@@ -0,0 +1,55 @@
+package org.lotka.bp.interactors.recipe_list
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import org.lotka.bp.cache.RecipeDao
+import org.lotka.bp.cache.model.RecipeEntityMapper
+import org.lotka.bp.domain.data.DataState
+import org.lotka.bp.domain.model.Recipe
+import org.lotka.bp.util.RECIPE_PAGINATION_PAGE_SIZE
+
+/**
+ * Restore a list of recipes after process death.
+ */
+class RestoreRecipes(
+    private val recipeDao: RecipeDao,
+    private val entityMapper: RecipeEntityMapper,
+) {
+    fun execute(
+        page: Int,
+        query: String
+    ): Flow<DataState<List<Recipe>>> = flow {
+        try {
+            emit(DataState.loading())
+
+            // just to show pagination, api is fast
+            delay(1000)
+
+            // query the cache
+            val cacheResult = if (query.isBlank()) {
+                recipeDao.restoreAllRecipes(
+                    pageSize = RECIPE_PAGINATION_PAGE_SIZE,
+                    page = page
+                )
+            } else {
+                recipeDao.restoreRecipes(
+                    query = query,
+                    pageSize = RECIPE_PAGINATION_PAGE_SIZE,
+                    page = page
+                )
+            }
+
+            // emit List<Recipe> from cache
+            val list = entityMapper.fromEntityList(cacheResult)
+            emit(DataState.success(list))
+
+        } catch (e: Exception) {
+            emit(DataState.error<List<Recipe>>(e.message ?: "Unknown Error"))
+        }
+    }
+}
+
+
+
+
