dc7f4830e9b6ea48d9c5cbca8d89a7c6f1d7e02b
Mobin Jafari <admin@lotka.org>
2023-05-26 14:55:57 +0330

    Add New Files

---
+++ app/src/main/java/org/lotka/bp/presentation/components/RecipeCard.kt
@@ -0,0 +1,35 @@
+package org.lotka.bp.presentation.components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.lotka.bp.domain.model.Recipe
+
+@ExperimentalCoroutinesApi
+@Composable
+fun RecipeCard(
+    recipe: Recipe,
+    onClick: () -> Unit,
+) {
+    Card(
+        shape = MaterialTheme.shapes.small,
+        modifier = Modifier
+            .padding(
+                bottom = 6.dp,
+                top = 6.dp,
+            )
+            .fillMaxWidth()
+            .clickable(onClick = onClick),
+    ) {
+
+    }
+}
+
+
