13efde649b723f27189aae80db3819defa2201cc
Mobin Jafari <admin@lotka.org>
2023-05-26 14:56:30 +0330

    Add New Files

---
+++ app/src/main/java/org/lotka/bp/network/RetrofitService.kt
@@ -0,0 +1,35 @@
+package org.lotka.bp.network
+
+import org.lotka.bp.network.model.RecipeDto
+import org.lotka.bp.network.response.RecipeSearchResponse
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+
+
+interface RecipeService {
+
+    @GET("search")
+    suspend fun search(
+        @Header("Authorization") token: String,
+        @Query("page") page: Int,
+        @Query("query") query: String
+    ): RecipeSearchResponse
+
+    @GET("get")
+    suspend fun get(
+        @Header("Authorization") token: String,
+        @Query("id") id: Int
+    ): RecipeDto
+}
+
+
+
+
+
+
+
+
+
+
+
