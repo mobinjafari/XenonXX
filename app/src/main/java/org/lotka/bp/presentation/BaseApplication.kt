3e983f4b3e8cec9f64a8c3477300959e9f9ba811
Mobin Jafari <admin@lotka.org>
2023-06-04 18:43:33 +0330

    Implemented Crane Dashboard

--- app/src/main/java/org/lotka/bp/presentation/BaseApplication.kt
+++ app/src/main/java/org/lotka/bp/presentation/BaseApplication.kt
@@ -1,10 +1,27 @@
 package org.lotka.bp.presentation
 
 import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.compose.AsyncImage
 import dagger.hilt.android.HiltAndroidApp
+import org.lotka.bp.presentation.util.UnsplashSizingInterceptor
 
 @HiltAndroidApp
-class BaseApplication : Application()
+class BaseApplication : Application(), ImageLoaderFactory {
+
+    /**
+     * Create the singleton [ImageLoader].
+     * This is used by [AsyncImage] to load images in the app.
+     */
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader.Builder(this)
+            .components {
+                add(UnsplashSizingInterceptor)
+            }
+            .build()
+    }
+}
 
 
 
