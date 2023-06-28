981b4887f6489b245f6d1f0b025887a43e30b81e
Mobin Jafari <admin@lotka.org>
2023-06-28 20:29:37 +0330

    Room Database Refactor

--- app/src/main/java/org/lotka/bp/cache/database/AppDatabase.kt
+++ app/src/main/java/org/lotka/bp/cache/database/AppDatabase.kt
@@ -5,12 +5,12 @@ import androidx.room.RoomDatabase
 import org.lotka.bp.cache.RecipeDao
 import org.lotka.bp.cache.model.RecipeEntity
 
-@Database(entities = [RecipeEntity::class ], version = 1)
-abstract class AppDatabase: RoomDatabase() {
+@Database(entities = [RecipeEntity::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
 
     abstract fun recipeDao(): RecipeDao
 
-    companion object{
+    companion object {
         val DATABASE_NAME: String = "recipe_db"
     }
 
