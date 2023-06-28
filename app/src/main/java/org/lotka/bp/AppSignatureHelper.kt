9e15b99019c96d86f43f2d620fff462d964836d7
Mobin Jafari <admin@lotka.org>
2023-06-28 20:27:11 +0330

    Gradle class path removed and Improved by new gradle 8

---
+++ app/src/main/java/org/lotka/bp/AppSignatureHelper.kt
@@ -0,0 +1,83 @@
+package org.lotka.bp
+
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.pm.PackageManager
+import android.util.Base64
+import android.util.Log
+import java.nio.charset.StandardCharsets
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+import java.util.*
+
+/*
+*     AppSignatureHelper.kt
+*
+*     Created by Atiar Talukdar on 2022/09/15.
+*/
+
+
+//TODO: Warning: Make sure to delete this class before release build for play store / production
+class AppSignatureHelper(context: Context?) :
+    ContextWrapper(context) {// Get all package signatures for the current package
+
+    // For each signature create a compatible hash
+    /**
+     * Get all the app signatures for the current package
+     *
+     * @return
+     *
+     *
+     */
+    val appSignatures: ArrayList<String>
+        get() {
+            val appCodes = ArrayList<String>()
+            try {
+                // Get all package signatures for the current package
+                val packageName = packageName
+                val packageManager = packageManager
+                val signatures = packageManager.getPackageInfo(
+                    packageName,
+                    PackageManager.GET_SIGNATURES
+                ).signatures
+
+                // For each signature create a compatible hash
+                for (signature in signatures) {
+                    val hash = hash(packageName, signature.toCharsString())
+                    if (hash != null) {
+                        appCodes.add(String.format("%s", hash))
+                    }
+                }
+            } catch (e: PackageManager.NameNotFoundException) {
+                Log.e(TAG, "Unable to find package to obtain hash.", e)
+            }
+            return appCodes
+        }
+
+    companion object {
+        val TAG = AppSignatureHelper::class.java.simpleName
+        private const val HASH_TYPE = "SHA-256"
+        const val NUM_HASHED_BYTES = 9
+        const val NUM_BASE64_CHAR = 11
+        private fun hash(packageName: String, signature: String): String? {
+            val appInfo = "$packageName $signature"
+            try {
+                val messageDigest = MessageDigest.getInstance(HASH_TYPE)
+                messageDigest.update(appInfo.toByteArray(StandardCharsets.UTF_8))
+                var hashSignature = messageDigest.digest()
+
+                // truncated into NUM_HASHED_BYTES
+                hashSignature = Arrays.copyOfRange(hashSignature, 0, NUM_HASHED_BYTES)
+                // encode into Base64
+                var base64Hash =
+                    Base64.encodeToString(hashSignature, Base64.NO_PADDING or Base64.NO_WRAP)
+                base64Hash = base64Hash.substring(0, NUM_BASE64_CHAR)
+                Log.d(TAG, String.format("pkg: %s -- hash: %s", packageName, base64Hash))
+                return base64Hash
+            } catch (e: NoSuchAlgorithmException) {
+                Log.e(TAG, "hash:NoSuchAlgorithm", e)
+            }
+            return null
+        }
+    }
+}
